8fd697613dcb4688a24049912f691681c8cf87fe
Greate-Seagull <h0274.hmt@gmail.com>
2026-09-22 07:55:23 +0700

    chore: setup configuration for spring boot implementation of idempotency service

---
+++ idempotency-spring-boot-starter/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("org.springframework.boot") version "4.1.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+dependencies {
+    api(project(":idempotency-api"))
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation("tools.jackson.core:jackson-databind")
+    testImplementation(testFixtures(project(":idempotency-api")))
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-redis-test")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("com.redis:testcontainers-redis")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
