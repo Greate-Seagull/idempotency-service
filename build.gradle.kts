93262554db9a8ea7675896f838eea5661e51e383
Greate-Seagull <h0274.hmt@gmail.com>
2026-09-24 15:43:59 +0700

    fix-security: add lockfiles to lock dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,20 +11,20 @@ sonar {
     }
 }
 
-subprojects {
-    plugins.apply("java")
-    plugins.apply("java-library")
-    group = "com.hungvers.idempotency"
-    version = "0.0.1"
+    subprojects {
+        plugins.apply("java")
+        plugins.apply("java-library")
+        group = "com.hungvers.idempotency"
+        version = "0.0.1"
 
-    repositories { mavenCentral() }
+        repositories { mavenCentral() }
 
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        java {
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+
+        dependencyLocking {
+            lockAllConfigurations()
+        }
     }
-
-    dependencyLocking {
-        lockAllConfigurations()
-    }
-}
